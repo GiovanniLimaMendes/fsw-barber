b0c8de9df7ef260c5154b7af3f30312d578222a1
giovanni lima mendes <giovannilmendes@gmail.com>
2025-05-24 16:01:28 -0300

    feat: Add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-import { Button } from "./_components/ui/button";
+import { Button } from "./_components/ui/button"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Test</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Giovanni!</h2>
+        <p>Sábado, 24 de Maio.</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
